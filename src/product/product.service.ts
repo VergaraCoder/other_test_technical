c5893674691b7bbcf2a8f4d45d3edae28d841472
vergaraCoder <jhonatanvergara072@gmail.com>
2024-10-07 10:00:11 -0500

    feat: creation of products methods

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,26 +1,87 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from './entities/product.entity';
+import { Repository } from 'typeorm';
+import { manageError } from 'src/common/erros/custom/custom.error';
 
 @Injectable()
 export class ProductService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+
+  constructor(
+    @InjectRepository(Product)
+    private productRepository:Repository<Product>
+  ){}
+
+  async create(createProductDto: CreateProductDto) {
+    try{
+      const dataProduct=this.productRepository.create(createProductDto);
+      await this.productRepository.save(dataProduct);
+      return dataProduct;
+    }catch(err:any){
+      throw err;
+    }
   }
 
-  findAll() {
-    return `This action returns all product`;
+  async findAll(querys?:any) {
+    try{
+      const products=await this.productRepository.find();
+      if(products.length==0){
+        throw new manageError({
+          type:"NOT_FOUND",
+          message:"DOES THERE ARE PRODUCTS"
+        });
+      } 
+      return products;
+    }catch(err:any){
+      throw manageError.signedError(err.message);
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: string) {
+    try{
+      const product=await this.productRepository.findOneBy({id});
+      if(!product){
+        throw new manageError({
+          type:"NOT_FOUND",
+          message:"DOES THERE ARE PRODUCT"
+        });
+      } 
+      return product;
+    }catch(err:any){
+      throw manageError.signedError(err.message);
+    }
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: number, updateProductDto: UpdateProductDto) {
+    try{
+      const {affected}=await this.productRepository.update(id,updateProductDto);
+      if(affected==0){
+        throw new manageError({
+          type:"NOT_FOUND",
+          message:"FAILDED TO UPDATE PRODUCT"
+        });
+      } 
+      return "Product perfectly upodated";
+    }catch(err:any){
+      throw manageError.signedError(err.message);
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: string) {
+
+    try{
+      const {affected}=await this.productRepository.delete(id);
+      if(affected==0){
+        throw new manageError({
+          type:"NOT_FOUND",
+          message:"FAILDED TO DELETED PRODUCT"
+        });
+      } 
+      return "Product perfectly deleted";
+    }catch(err:any){
+      throw manageError.signedError(err.message);
+    }
   }
 }
